bf36242bbaaf60a001e287b34a08637bce48fefc
ksyxt0700379 <452505518@qq.com>
2021-08-18 17:58:31 +0800

    增加了一些可用的图片匹配方式

--- test.py
+++ test.py
@@ -61,7 +61,14 @@ if __name__ == "__main__":
     # 取出Alpha通道
     #alpha = template[:,:,3]
     #template = cv2.cvtColor(template, cv2.COLOR_BGRA2GRAY)
-    # 模板匹配，将alpha作为mask，TM_CCORR_NORMED方法的计算结果范围为[0, 1]，越接近1越匹配
+    # 模板匹配，将alpha作为mask，TM_CCORR_NORMED方法的计算结果范围为[0, 1]，越接近1越匹配 TM_CCOEFF_NORMED
+    # CV_TM_SQDIFF 平方差匹配法：该方法采用平方差来进行匹配；最好的匹配值为0；匹配越差，匹配值越大。
+    # CV_TM_CCORR 相关匹配法：该方法采用乘法操作；数值越大表明匹配程度越好。
+    # CV_TM_CCOEFF 相关系数匹配法：1表示完美的匹配；-1表示最差的匹配。
+    # CV_TM_SQDIFF_NORMED 归一化平方差匹配法
+    # CV_TM_CCORR_NORMED 归一化相关匹配法
+    # CV_TM_CCOEFF_NORMED 归一化相关系数匹配法
+
     result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
     print(cv2.minMaxLoc(result))
     # 获取结果中最大值和最小值以及他们的坐标
